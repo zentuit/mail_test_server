52e58bc49fa9fcb74ca97432bda5f34f9d6fa00e
Tom Jenkins <jenkins.tom@gmail.com>
2011-04-13 10:00:25 -0400

    changed to instance and refactoring out available options to one place

--- src-test/com/devis/mailserver/RunServerTests.groovy
+++ src-test/com/devis/mailserver/RunServerTests.groovy
@@ -2,14 +2,22 @@ package com.devis.mailserver;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
 
 class RunServerTests {
+
+	def runServer
+
+	@Before
+	public void setUp() throws Exception {
+		runServer = new RunServer()
+	}
 
 	@Test
 	public void test_port_parameter() {
 		def param = ["-p", "6677"] as String[]
 		
-		def results = RunServer.processParameters(param)
+		def results = runServer.processParameters(param)
 		
 		assert 6677 == results.port
 		assert !results.sleep
@@ -19,7 +27,7 @@ class RunServerTests {
 	public void test_port_parameter_full_name() {
 		def param = ["-port", "6677"] as String[]
 		
-		def results = RunServer.processParameters(param)
+		def results = runServer.processParameters(param)
 		
 		assert 6677 == results.port
 		assert !results.sleep
@@ -29,7 +37,7 @@ class RunServerTests {
 	public void test_port_parameter_with_no_parameter() {
 		def param = [] as String[]
 		
-		def results = RunServer.processParameters(param)
+		def results = runServer.processParameters(param)
 		
 		assert !results.port
 		assert !results.sleep
@@ -39,7 +47,7 @@ class RunServerTests {
 	public void test_sleep_parameter() {
 		def param = ["-s", "5000"] as String[]
 		
-		def results = RunServer.processParameters(param)
+		def results = runServer.processParameters(param)
 		
 		assert 5000 == results.sleep
 		assert !results.port
@@ -50,7 +58,7 @@ class RunServerTests {
 		def path = "./my/path/myconfig.groovy"
 		def param = ["-c", path] as String[]
 		
-		def results = RunServer.processParameters(param)
+		def results = runServer.processParameters(param)
 		
 		assert path == results.config
 	}
@@ -60,7 +68,7 @@ class RunServerTests {
 		def path = "./my/path/myconfig.groovy"
 		def param = ["-config", path] as String[]
 		
-		def results = RunServer.processParameters(param)
+		def results = runServer.processParameters(param)
 		
 		assert path == results.config
 	}
@@ -69,7 +77,7 @@ class RunServerTests {
 	public void test_config_file_port_setting() {
 		def filename = "./resources-test/just_port.groovy"
 		
-		def results = RunServer.processConfigFile(filename)
+		def results = runServer.processConfigFile(filename)
 		
 		assert 7777 == results.port
 		
@@ -79,7 +87,7 @@ class RunServerTests {
 	public void test_config_file_sleep_setting() {
 		def filename = "./resources-test/just_sleep.groovy"
 		
-		def results = RunServer.processConfigFile(filename)
+		def results = runServer.processConfigFile(filename)
 		
 		assert 1000 == results.sleep
 		
--- src/com/devis/mailserver/RunServer.groovy
+++ src/com/devis/mailserver/RunServer.groovy
@@ -1,13 +1,25 @@
 package com.devis.mailserver
 
+import org.apache.commons.cli.Option
+
 class RunServer {
 	private static String defaultPort = "5555"
 	private static String defaultSleep = "2000" //milliseconds
 	private static String defaultConfig = "./resources/config.groovy"
+	
+	def private availableOptions = [
+			[opt: "h", longOpt: 'help', description:'Show usage information'],
+			[opt: "p", longOpt: 'port', args:1, argName:'port', description:"Port to listen on (default ${defaultPort}})"],
+			[opt: "s", longOpt: 'sleep', args:1, argName:'sleep', description:"Number of milliseconds to wait while polling (default ${defaultSleep})"],
+			[opt: "c", longOpt: 'config', args:1, argName: 'filename', description:"Configuration file (default ${defaultConfig})"],
+		]
+
 
 	public static void main(String [] args) {
 		
-		def options = determineServerParams(args)
+		def server = new RunServer()
+		
+		def options = server.determineServerParams(args)
 		if (!options) { return }
 
 		def port = (options.p) ?: defaultPort
@@ -30,15 +42,23 @@ class RunServer {
 		def options = configOptions + cmdLineOptions
 	}
 	
-	def static processParameters(String [] args) {
+	def processParameters(String [] args) {
 		def cli = new CliBuilder(usage: 'RunServer [-h] [-p port] [-s sleep (ms)] [-c config file')
-		
-		cli.with {
-			h longOpt: 'help', 'Show usage information'
-			p longOpt: 'port', args:1, argName:'port', "Port to listen on (default ${defaultPort}})"
-			s longOpt: 'sleep', args:1, argName:'sleep', "Number of milliseconds to wait while polling (default ${defaultSleep})"
-			c longOpt: 'config', args:1, argName: 'filename', "Configuration file (default ${defaultConfig})"
+
+		availableOptions.each {
+			def opt = new Option(it.opt, it.description)
+			if (it.longOpt) { opt.longOpt = it.longOpt }
+			if (it.args) { opt.args = it.args }
+			if (it.argName) { opt.argName = it.argName }
+			cli << opt
 		}
+		
+//		cli.with {
+//			h longOpt: 'help', 'Show usage information'
+//			p longOpt: 'port', args:1, argName:'port', "Port to listen on (default ${defaultPort}})"
+//			s longOpt: 'sleep', args:1, argName:'sleep', "Number of milliseconds to wait while polling (default ${defaultSleep})"
+//			c longOpt: 'config', args:1, argName: 'filename', "Configuration file (default ${defaultConfig})"
+//		}
 		
 		def options = cli.parse(args)
 		
@@ -52,13 +72,13 @@ class RunServer {
 
 	} 
 	
-	def static convertToInteger(options) {
+	def convertToInteger(options) {
 		def port = (options.p) ? Integer.valueOf(options.p) : null
 		def sleep = (options.s) ? Integer.valueOf(options.s) : null
 		return [port:port, sleep:sleep]
 	}
 	
-	def static processConfigFile(String filename) {
+	def processConfigFile(String filename) {
 		def file = new File(filename).toURL()
 		def config = new ConfigSlurper().parse(file)
 		
